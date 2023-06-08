9d99e8ebad8c48e68a8e1ba6984d1c960b98c0a2
rajiv gardas <gardasrajiv@gmail.com>
2023-06-08 17:19:39 -0500

    changed name back to sextant and final

--- sextant/src/Banner.js
+++ sextant/src/Banner.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './Banner.css';
+import React from "react";
+import "./Banner.css";
 
 function Banner() {
-  return <h1 className="Banner">NetView - Network Dashboard</h1>;
+  return <h1 className="Banner">Sextant - Network Dashboard</h1>;
 }
 
 export default Banner;
